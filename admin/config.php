8243b963d846239431c99cdf5d97ad9e0dc191d4
Nick Liang <nliang@hcf.com.au>
2018-06-26 11:32:18 +1000

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.beutii.net/admin/');
+define('HTTP_CATALOG', 'http://www.beutii.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.beutii.net/admin/');
+define('HTTPS_CATALOG', 'http://www.beutii.net/');
+
+define('DIR_PREFIX', 'C:\Projects');///home/wwwroot
+
+// DIR
+define('DIR_APPLICATION', DIR_PREFIX . '/beutii.net/admin/');
+define('DIR_SYSTEM', DIR_PREFIX . '/beutii.net/system/');
+define('DIR_IMAGE', DIR_PREFIX . '/beutii.net/image/');
+define('DIR_LANGUAGE', DIR_PREFIX . '/beutii.net/admin/language/');
+define('DIR_TEMPLATE', DIR_PREFIX . '/beutii.net/admin/view/template/');
+define('DIR_CONFIG', DIR_PREFIX . '/beutii.net/system/config/');
+define('DIR_CACHE', DIR_PREFIX . '/beutii.net/system/storage/cache/');
+define('DIR_DOWNLOAD', DIR_PREFIX . '/beutii.net/system/storage/download/');
+define('DIR_LOGS', DIR_PREFIX . '/beutii.net/system/storage/logs/');
+define('DIR_MODIFICATION', DIR_PREFIX . '/beutii.net/system/storage/modification/');
+define('DIR_UPLOAD', DIR_PREFIX . '/beutii.net/system/storage/upload/');
+define('DIR_CATALOG', DIR_PREFIX . '/beutii.net/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'SwdT5byZA3MtfLM7');
+define('DB_PASSWORD', 'SwdT5byZA3MtfLM7');
+define('DB_DATABASE', 'SwdT5byZA3MtfLM7');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.beutii.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.beutii.net/');
+
+define('DIR_PREFIX', 'C:\Projects');///home/wwwroot
+
+// DIR
+define('DIR_APPLICATION', DIR_PREFIX . '/beutii.net/catalog/');
+define('DIR_SYSTEM', DIR_PREFIX . '/beutii.net/system/');
+define('DIR_IMAGE', DIR_PREFIX . '/beutii.net/image/');
+define('DIR_LANGUAGE', DIR_PREFIX . '/beutii.net/catalog/language/');
+define('DIR_TEMPLATE', DIR_PREFIX . '/beutii.net/catalog/view/theme/');
+define('DIR_CONFIG', DIR_PREFIX . '/beutii.net/system/config/');
+define('DIR_CACHE', DIR_PREFIX . '/beutii.net/system/storage/cache/');
+define('DIR_DOWNLOAD', DIR_PREFIX . '/beutii.net/system/storage/download/');
+define('DIR_LOGS', DIR_PREFIX . '/beutii.net/system/storage/logs/');
+define('DIR_MODIFICATION', DIR_PREFIX . '/beutii.net/system/storage/modification/');
+define('DIR_UPLOAD', DIR_PREFIX . '/beutii.net/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'SwdT5byZA3MtfLM7');
+define('DB_PASSWORD', 'SwdT5byZA3MtfLM7');
+define('DB_DATABASE', 'SwdT5byZA3MtfLM7');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
