4031d3fc1771498faf2691e5a7907007fbcb6a90
Nick Liang <nliang@hcf.com.au>
2018-06-27 16:06:33 +1000

    adding SKU into $data

--- system/storage/modification/catalog/controller/product/product.php
+++ system/storage/modification/catalog/controller/product/product.php
@@ -297,6 +297,8 @@ class ControllerProductProduct extends Controller {
 			$data['manufacturer'] = $product_info['manufacturer'];
 			$data['manufacturers'] = $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $product_info['manufacturer_id']);
 			$data['model'] = $product_info['model'];
+			//Adding Sku into the model view
+			$data['sku'] = $product_info['sku'];
 			$data['reward'] = $product_info['reward'];
 			$data['points'] = $product_info['points'];
 			$data['description'] = html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8');
